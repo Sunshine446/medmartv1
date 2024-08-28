32037bf2d6773c33e05863543622ac31c8fd3076
sunshine446 <sunshinenavarr5@gmail.com>
2024-08-28 14:18:48 +0800

    translation 1

--- lang/fil/message.php
+++ lang/fil/message.php
@@ -25,6 +25,48 @@ return [
 
     //product related translation
     'product' => 'Produkto',
-    'product_count' => 'Bilang ng produkto'
+    'product_count' => 'Bilang ng produkto',
+
+    //dashboard
+    'total_sales' => 'Kabuuang Benta',
+    'sales_today' => 'Kabuuang Benta Ngayon',
+    'inventory_level' => 'Antas ng Imbentaryo',
+    'highest_inventory' => 'Pinakamataas na Imbentaryo',
+    'lowest_inventory' => 'Pinakamababang Imbentaryo',
+    'products_expired' => 'Mga Produktong expire',
+    'showing_expired' => 'Ipinapakita ang mga expire',
+    'no_expired' => 'Walang expire',
+    '' => 'Sales Count',
+
+    // '' => 'Product Count',
+    'supplier_count' => 'Bilang ng mga Supplier',
+
+    //product list
+    'total_products' => 'Kabuuang Produkto',
+    'product_name' => 'Pangalan ng Produkto',
+    'generic' => 'Pangkalahatan',
+    'category' => 'Kategorya',
+    'description' => 'Paglalarawan',
+    'price' => 'Presyo',
+    'action' => 'Aksyon',
+
+    //product batches
+    'total_batches' => 'Kabuuang Batch',
+    'batch_created' => 'Batch na Nilikha',
+    'batch_number' => 'Numero ng Batch',
+    'expiration_date' => 'Petsa ng Pagka-expire',
+    'supplier_price' => 'Presyo ng Supplier',
+    'recieved_date' => 'Petsa ng Pagtanggap',
+    'created_at' => 'Nilikha Noong',
+    'updated_at' => 'Nai-update Noong',
+
+    //inventory list
+    '' => 'Kabuuang Item',
+
+
+
+]
+
+?>
 ]
 ?>
